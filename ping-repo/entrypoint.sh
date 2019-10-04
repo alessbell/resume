e5e6434072e5d2965328acc977244b51e58de19b
Alessia Bellisario <alessia@breather.com>
2019-10-04 07:33:45 -0400

    Finalize action

--- ping-repo/entrypoint.sh
+++ ping-repo/entrypoint.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
 main() {
-  # echo 'in entrypoint.sh'
-  # echo 'Node version:' $(node -v)
-  # echo 'NPM version:' $(npm -v)
-  # echo 'Git version:' $(git --version)
-  # echo 'output of pwd:' && pwd
-  # echo 'output of ls:' && ls
-  # echo 'these are arguments set to the workflow' && echo ${*}
-  echo 'token' ${GITHUB_TOKEN}
-  echo 'github repo' ${GITHUB_REPOSITORY}
-  echo 'repo' ${REPO}
-
   curl -XPOST -H "Accept: application/vnd.github.everest-preview+json" \
   -H "Content-Type: application/json" \
   -H "Authorization: token ${GITHUB_TOKEN}" \
   "https://api.github.com/repos/${REPO}/dispatches" \
-  --data '{"event_type": "my_custom_webhook"}'
+  --data '{"event_type": "update_resume"}'
 }
 
 main
