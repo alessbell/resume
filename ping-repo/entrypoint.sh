0a9ba264130f4a9d579b0ce71a6dabe613531ca0
Alessia Bellisario <alessia@breather.com>
2019-10-03 23:01:00 -0400

    One more time x6

--- ping-repo/entrypoint.sh
+++ ping-repo/entrypoint.sh
@@ -1,25 +1,3 @@
-# #!/bin/bash
-
-# main() {
-#   # echo 'in entrypoint.sh'
-#   # echo 'Node version:' $(node -v)
-#   # echo 'NPM version:' $(npm -v)
-#   # echo 'Git version:' $(git --version)
-#   # echo 'output of pwd:' && pwd
-#   # echo 'output of ls:' && ls
-#   # echo 'these are arguments set to the workflow' && echo ${*}
-#   echo 'token' ${GITHUB_TOKEN}
-
-#   curl -XPOST -H "Accept: application/vnd.github.everest-preview+json" \
-#   -H "Content-Type: application/json" \
-#   -H "Authorization: token ${GITHUB_TOKEN}" \
-#   "https://api.github.com/repos/${GITHUB_REPOSITORY}/dispatches" \
-#   --data '{"event_type": "my_custom_webhook"}'
-# }
-
-# main
-
-
 #!/bin/sh -l
 
 sh -c "curl -XPOST -H \"Accept: \
