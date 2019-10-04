200c8eed4d75b6eb06767a84fdc59183221ef4a2
Alessia Bellisario <alessia@breather.com>
2019-10-03 23:25:59 -0400

    One more time x7

--- ping-repo/entrypoint.sh
+++ ping-repo/entrypoint.sh
@@ -1,8 +1,21 @@
-#!/bin/sh -l
+#!/bin/bash
 
-sh -c "curl -XPOST -H \"Accept: \
-application/vnd.github.everest-preview+json\"  \
--H \"Content-Type: application/json\" \
--H \"Authorization: token ${GITHUB_TOKEN}\" \
-\"https://api.github.com/repos/${GITHUB_REPOSITORY}/dispatches\" \
---data '{\"event_type\": \"my_custom_webhook\"}'"
+main() {
+  # echo 'in entrypoint.sh'
+  # echo 'Node version:' $(node -v)
+  # echo 'NPM version:' $(npm -v)
+  # echo 'Git version:' $(git --version)
+  # echo 'output of pwd:' && pwd
+  # echo 'output of ls:' && ls
+  # echo 'these are arguments set to the workflow' && echo ${*}
+  echo 'token' ${GITHUB_TOKEN}
+  echo 'repo' ${GITHUB_REPOSITORY}
+
+  curl -XPOST -H "Accept: application/vnd.github.everest-preview+json" \
+  -H "Content-Type: application/json" \
+  -H "Authorization: token ${GITHUB_TOKEN}" \
+  "https://api.github.com/repos/${GITHUB_REPOSITORY}/dispatches" \
+  --data '{"event_type": "my_custom_webhook"}'
+}
+
+main
