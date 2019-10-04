3b110aed7edc7110cb2e6423447ff293f10b61c0
Alessia Bellisario <alessia@breather.com>
2019-10-03 22:56:42 -0400

    One more time x5

--- ping-repo/entrypoint.sh
+++ ping-repo/entrypoint.sh
@@ -1,28 +1,30 @@
-#!/bin/bash
+# #!/bin/bash
 
-main() {
-  # echo 'in entrypoint.sh'
-  # echo 'Node version:' $(node -v)
-  # echo 'NPM version:' $(npm -v)
-  # echo 'Git version:' $(git --version)
-  # echo 'output of pwd:' && pwd
-  # echo 'output of ls:' && ls
-  # echo 'these are arguments set to the workflow' && echo ${*}
+# main() {
+#   # echo 'in entrypoint.sh'
+#   # echo 'Node version:' $(node -v)
+#   # echo 'NPM version:' $(npm -v)
+#   # echo 'Git version:' $(git --version)
+#   # echo 'output of pwd:' && pwd
+#   # echo 'output of ls:' && ls
+#   # echo 'these are arguments set to the workflow' && echo ${*}
+#   echo 'token' ${GITHUB_TOKEN}
 
-  curl -X POST -H "Accept: application/vnd.github.everest-preview+json" \
-  -H "Content-Type: application/json" \
-  -H "Authorization: token ${GITHUB_TOKEN}" \
-  "https://api.github.com/repos/${GITHUB_REPOSITORY}/dispatches" \
-  --data '{"event_type": "my_custom_webhook"}'
-}
+#   curl -XPOST -H "Accept: application/vnd.github.everest-preview+json" \
+#   -H "Content-Type: application/json" \
+#   -H "Authorization: token ${GITHUB_TOKEN}" \
+#   "https://api.github.com/repos/${GITHUB_REPOSITORY}/dispatches" \
+#   --data '{"event_type": "my_custom_webhook"}'
+# }
 
-main
+# main
 
 
-# #!/bin/sh -l
-# sh -c "curl -XPOST -H \"Accept: \
-# application/vnd.github.everest-preview+json\"  \
-# -H \"Content-Type: application/json\" \
-# -H \"Authorization: token ${PA_TOKEN}\" \
-# \"https://api.github.com/repos/${GITHUB_REPOSITORY}/dispatches\" \
-# --data '{\"event_type\": \"my_custom_webhook\"}'"
+#!/bin/sh -l
+
+sh -c "curl -XPOST -H \"Accept: \
+application/vnd.github.everest-preview+json\"  \
+-H \"Content-Type: application/json\" \
+-H \"Authorization: token ${GITHUB_TOKEN}\" \
+\"https://api.github.com/repos/${GITHUB_REPOSITORY}/dispatches\" \
+--data '{\"event_type\": \"my_custom_webhook\"}'"
